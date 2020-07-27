a217a3821439937cd9c7564dd6abc5cac0dd7b29
Sarang S <email.sarangs@gmail.com>
2020-07-27 11:51:33 +0530

    Update play.js

--- include/play.js
+++ include/play.js
@@ -1,10 +1,9 @@
 const ytdlDiscord = require("ytdl-core-discord");
-const scdl = require("soundcloud-downloader");
 const { canModifyQueue } = require("./util");
 
 module.exports = {
   async play(song, message) {
-    const { PRUNING, SOUNDCLOUD_CLIENT_ID } = require("../config.json");
+    const { PRUNING } = require("../config.json");
     const queue = message.client.queue.get(message.guild.id);
 
     if (!song) {
@@ -18,10 +17,6 @@ module.exports = {
     try {
       if (song.url.includes("youtube.com")) {
         stream = await ytdlDiscord(song.url, { highWaterMark: 1 << 25 });
-      } else if (song.url.includes("soundcloud.com") && SOUNDCLOUD_CLIENT_ID) {
-        const info = await scdl.getInfo(song.url, SOUNDCLOUD_CLIENT_ID);
-        const opus = scdl.filterMedia(info.media.transcodings, { format: scdl.FORMATS.OPUS });
-        stream = await scdl.downloadFromURL(opus[0].url, SOUNDCLOUD_CLIENT_ID);
       }
     } catch (error) {
       if (queue) {
