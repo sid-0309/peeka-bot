0074d792184eb32eb6f737d5552b5b5f3129596c
sid-0309 <sid-0309@users.noreply.github.com>
2020-07-28 15:38:35 +0530

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 
-const catAPI = 'https://api.thecatapi.com/v1/images/search'
+const catAPI = 'https://api.thecatapi.com/v1/images/search?api_key=b34a49cd-7313-442a-9c87-6f8bf7fb2f1a'
 
 module.exports = {
     name: 'cat',
