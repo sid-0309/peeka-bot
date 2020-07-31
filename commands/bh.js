1118638e8ca62ac2ab2cd110e59ff1d84a261986
sid-0309 <sid-0309@users.noreply.github.com>
2020-07-31 20:19:18 +0530

    Create bh.js

---
+++ commands/bh.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'bh',
+    description: 'Spam @disgusty',
+    execute(message) {
+        for (let i = 0; i<=10;i++){
+            message.channel.send("<@478616498802524161>")
+        }
+    }
+}
