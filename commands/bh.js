8e8b3b017ef28aff1ca7757295a74b85843fa8a2
sid-0309 <sid-0309@users.noreply.github.com>
2020-08-15 10:05:39 +0530

    Delete bh.js

--- commands/bh.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'bh',
-    description: 'Spam @disgusty',
-    execute(message) {
-        for (let i = 0; i<=10;i++){
-            message.channel.send("<@478616498802524161>")
-        }
-    }
-}
